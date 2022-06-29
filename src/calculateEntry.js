879a9b1f3f9a094bed09111a2a340a500bdc871c
Camila Wolter <camila0wolter@gmail.com>
2022-06-29 18:29:08 -0300

    requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,23 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  return {
+    child: entrants.filter((pessoa) => pessoa.age < 18).length,
+    adult: entrants.filter((pessoa) => pessoa.age >= 18 && pessoa.age < 50).length,
+    senior: entrants.filter((pessoa) => pessoa.age >= 50).length,
+  };
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (entrants === undefined || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+
+  const pessoa = countEntrants(entrants);
+  const childTotal = data.prices.child * pessoa.child;
+  const adultTotal = data.prices.adult * pessoa.adult;
+  const seniorTotal = data.prices.senior * pessoa.senior;
+  return childTotal + adultTotal + seniorTotal;
 }
 
 module.exports = {
