1ce511d1681dfac558baaf9806351a4bd56aa58e
Camila Wolter <camila0wolter@gmail.com>
2022-06-29 17:41:44 -0300

    requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,29 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
+function allAnimals() {
+  const animals = {};
+  species.forEach((animal) => {
+    animals[animal.name] = animal.residents.length;
+  });
+  return animals;
+}
+
 function countAnimals(animal) {
-  // seu código aqui
+  if (animal === undefined) {
+    return allAnimals();
+  }
+
+  const { specie, sex: gender = undefined } = animal;
+
+  if (gender === undefined) {
+    const animalName = species.find(({ name }) => name === specie).residents.length;
+    return animalName;
+  }
+
+  const animalGender = species.find(({ name }) => name === specie)
+    .residents.filter(({ sex }) => sex === gender).length;
+  return animalGender;
 }
 
 module.exports = countAnimals;
