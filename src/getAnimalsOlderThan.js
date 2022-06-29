ec90110be009a12a80eecc551598369f8f1c15dd
Camila Wolter <camila0wolter@gmail.com>
2022-06-29 16:10:14 -0300

    requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,11 @@
+const {
+  species,
+} = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const encontrarEspecie = species.find((specieAnimal) => specieAnimal.name === animal);
+  return encontrarEspecie.residents.every((resident) => resident.age >= age);
 }
 
 module.exports = getAnimalsOlderThan;
