567e0eea72abe2838621c0371513e6ae98f6fd7d
Camila Wolter <camila0wolter@gmail.com>
2022-06-29 17:08:53 -0300

    requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,22 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  return data.employees.some((info) => info.managers.some((check) => check === id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId)) {
+    const filtroManager = data.employees
+      .filter((elemento) => elemento.managers
+        .some((id) => id === managerId));
+
+    const listaManager = filtroManager
+      .map((employee) => `${employee.firstName} ${employee.lastName}`);
+
+    return listaManager;
+  }
+
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
 
 module.exports = { isManager, getRelatedEmployees };
