4e56232aa509cd11dbeefa37b8e280f40d07cd9f
Camila Wolter <camila0wolter@gmail.com>
2022-06-29 16:40:53 -0300

    requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,11 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (!employeeName) {
+    return {};
+  }
+  return employees.find((nom) => nom.firstName === employeeName || nom.lastName === employeeName);
 }
 
 module.exports = getEmployeeByName;
