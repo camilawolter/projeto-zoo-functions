bca5eb76e701d2f9e70bce9dd18399c9fb6d8082
Camila Wolter <camila0wolter@gmail.com>
2022-06-30 17:11:19 -0300

    requisito 12

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -1,3 +1,23 @@
 const getOpeningHours = require('../src/getOpeningHours');
 
-describe('Testes da função getOpeningHours', () => {});
+describe('Testes da função getOpeningHours', () => {
+  it('Retorna o objeto se não for passado argumentos', () => {
+    expect(getOpeningHours()).toEqual({
+      Tuesday: { open: 8, close: 6 },
+      Wednesday: { open: 8, close: 6 },
+      Thursday: { open: 10, close: 8 },
+      Friday: { open: 10, close: 8 },
+      Saturday: { open: 8, close: 10 },
+      Sunday: { open: 8, close: 8 },
+      Monday: { open: 0, close: 0 } });
+  });
+  it('Retorna "The zoo is closed" quando passado os argumentos "Monday" e "09:00-AM"', () => {
+    expect(getOpeningHours('Monday', '09:00-AM')).toEqual('The zoo is closed');
+  });
+  it('Retorna "The zoo is open" quando passados os argumentos "Tuesday" e "09:00-AM"', () => {
+    expect(getOpeningHours('Tuesday', '09:00-AM')).toEqual('The zoo is open');
+  });
+  it('Retorna "The zoo is closed" quando passados os argumentos "Wednesday" e "09:00-PM"', () => {
+    expect(getOpeningHours('Wednesday', '09:00-PM')).toEqual('The zoo is closed');
+  });
+});
