f1ea33350eb2ab5d35a43d744b781ab417e4c4ca
Camila Wolter <camila0wolter@gmail.com>
2022-06-30 12:57:29 -0300

    requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,19 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const encontarEmployee = data.employees.find((employee) => employee.id === id);
+
+  const especieId = encontarEmployee.responsibleFor[0];
+
+  const encontrarEspecie = data.species.find((specie) => specie.id === especieId);
+
+  const animalMaisVelho = encontrarEspecie.residents.reduce((velho, residente) => {
+    if (velho.age > residente.age) {
+      return velho;
+    } return residente;
+  });
+
+  return Object.values(animalMaisVelho);
 }
 
 module.exports = getOldestFromFirstSpecies;
