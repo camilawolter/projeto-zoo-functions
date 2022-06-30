1c976c8efcdd78171d426e655ec0f0b00e7f78de
Camila Wolter <camila0wolter@gmail.com>
2022-06-30 16:44:09 -0300

    requisito 11

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,48 @@
+const {
+  species,
+  employees,
+} = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+function nomeELocalizacaoAnimais(idAnimais) {
+  return species.reduce((listNomes, animal) => {
+    if (idAnimais.includes(animal.id)) {
+      listNomes.listAnimals.push(animal.name);
+      listNomes.listLocationAnimal.push(animal.location);
+    }
+    return listNomes;
+  }, {
+    listAnimals: [],
+    listLocationAnimal: [],
+  });
+}
+
+const employeesDados = () => employees.reduce((listEmployees, employee) => {
+  const listAnimals = nomeELocalizacaoAnimais(employee.responsibleFor);
+  const funcionario = {
+    id: employee.id,
+    fullName: `${employee.firstName} ${employee.lastName}`,
+    species: listAnimals.listAnimals,
+    locations: listAnimals.listLocationAnimal,
+  };
+  const lista = [...listEmployees, funcionario];
+  return lista;
+}, []);
+
+const retornaEmployee = (employeeId) => {
+  const funcionarios = employeesDados();
+  return funcionarios.find((element) => element.id === employeeId.id
+  || element.fullName.includes(employeeId.name));
+};
+
+function getEmployeesCoverage(param) {
+  const listEmployeesDados = employeesDados();
+  if (param === undefined) return listEmployeesDados;
+
+  const listEmployee = retornaEmployee(param);
+  if (!listEmployee) throw new Error('Informações inválidas');
+
+  return listEmployee;
 }
 
 module.exports = getEmployeesCoverage;
